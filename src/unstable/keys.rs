7573e5ddd0c1402e9a1799769ebdeff7e720b2ec
Nathan Musoke <nathan.musoke@gmail.com>
2017-10-13 19:00:49 +1300

    Add endpoint keys/query

--- src/unstable/keys.rs
+++ src/unstable/keys.rs
@@ -53,3 +53,58 @@ pub mod upload {
         signatures: String,
     }
 }
+
+/// [POST /_matrix/client/unstable/keys/query](https://matrix.org/speculator/spec/drafts%2Fe2e/client_server/unstable.html#post-matrix-client-unstable-keys-query)
+pub mod query {
+    use ruma_api_macros::ruma_api;
+    use ruma_identifiers::UserId;
+    use std::collections::HashMap;
+
+    type DeviceID = String;
+    type Homeserver = String;
+
+    ruma_api! {
+        metadata {
+            description: "Returns the current devices and identity keys for the given users.",
+            method: Method::Post,
+            name: "query",
+            path: "/_matrix/client/unstable/keys/query",
+            rate_limited: false,
+            requires_authentication: true,
+        }
+
+        request{
+            /// Time (in milliseconds) to wait when downloading keys from remote servers
+            ///
+            /// 10 seconds is the recommended default
+            // TODO: set default?
+            timeout: u64,
+            /// Users corresponding devices for which to get keys
+            pub device_keys: HashMap<UserId, Vec<DeviceID>>,
+        }
+
+        response{
+            /// Homeservers which could not be reached
+            failures: HashMap<Homeserver, String>,
+            /// Information on the queried devices
+            device_keys: HashMap<UserId, HashMap<DeviceID, DeviceKeys>>
+        }
+    }
+
+    /// Identity keys
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct DeviceKeys {
+        /// The ID of the user
+        user_id: UserId,
+        /// The ID of the device
+        device_id: DeviceID,
+        /// Supported algorithms
+        algorithms: Vec<String>,
+        /// Public identity keys
+        keys: HashMap<String, String>,
+        /// Signatures for the object
+        signatures: HashMap<String, HashMap<String, String>>,
+        /// Additional data added by intermediate servers
+        unsigned: String,
+    }
+}
